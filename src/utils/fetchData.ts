a10a32f98e88c8b3fc3b78a4978d44f6ea05bd18
Vojtech Benes <vojtech.benes@carvago.com>
2024-03-02 14:45:00 +0100

    remove unused functionalities

--- src/utils/fetchData.ts
+++
@@ -1,9 +0,0 @@
-import {supabase} from "@/utils/config.tsx";
-
-export const fetchRecordings = async () => {
-	const {data, error} = await supabase
-		.from('recordings')
-		.select()
-
-	return data
-}
