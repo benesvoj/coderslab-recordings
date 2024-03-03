15483a429cda6ce7e21b69d0b5774e68e8d2f148
Vojtech Benes <vojtech.benes@carvago.com>
2024-03-03 21:16:29 +0100

    form for edit recordings

--- src/components/EditRecordingDialog.tsx
+++ src/components/EditRecordingDialog.tsx
@@ -6,23 +6,36 @@ import {
 	DialogTitle,
 } from "@/components/ui/dialog.tsx";
 import {Button} from "@/components/ui/button.tsx";
-import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {Textarea} from "@/components/ui/textarea.tsx";
 import {Recording} from "@/App.tsx";
-import {FormEvent} from "react";
 import {supabase} from "@/utils/supabase.ts";
+import {Form, FormControl, FormField, FormItem, FormLabel} from "@/components/ui/form.tsx";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
 
-export const EditRecordingDialog = (props: { recording: Recording, isOpen: boolean, onOpenChange: (isOpen: boolean) =>  void }) => {
+export const EditRecordingDialog = (props: {
+	recording: Recording,
+	isOpen: boolean,
+	onOpenChange: (isOpen: boolean) => void
+}) => {
 
 	const {recording, isOpen, onOpenChange} = props
 
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		console.log('submit')
+	const form = useForm<z.infer<typeof schema>>({
+		resolver: zodResolver(schema),
+		defaultValues: {
+			title: recording.title,
+			description: recording.description,
+			url: recording.url
+		}
+	})
+
+	const onSubmit = async (values: z.infer<typeof  schema>) => {
 		const {error} = await supabase
 			.from('recordings')
-			.update({title: recording.title, description: recording.description})
+			.update({title: values.title, url: values.url, description: values.description})
 			.eq('id', recording.id)
 
 		if (error) throw error;
@@ -37,25 +50,59 @@ export const EditRecordingDialog = (props: { recording: Recording, isOpen: boole
 						Following fields are editable.
 					</DialogDescription>
 				</DialogHeader>
-				<form onSubmit={handleSubmit}>
-					<div className="flex flex-col gap-2">
-						<Label htmlFor="title" className='text-sm text-gray-400'>
-							Title
-						</Label>
-						<Input id="title" name="title" value={recording.title} onChange={e => recording.title = e.target.value} className="col-span-3"/>
-					</div>
-					<div className="flex flex-col gap-2">
-						<Label htmlFor='description' className='text-sm text-gray-400'>Description</Label>
-						<Textarea id='description' name="description" value={recording.description} className="col-span-3" rows={6} onChange={e => recording.title = e.target.value} />
-					</div>
-				<DialogFooter className='mt-4'>
-					<DialogClose asChild>
-						<Button variant='secondary'>Close</Button>
-					</DialogClose>
-					<Button variant='default' type='submit'>Save</Button>
-				</DialogFooter>
-				</form>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)}>
+						<FormField
+							name='title'
+							control={form.control}
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>Title</FormLabel>
+									<FormControl>
+										<Input {...field}/>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							name='url'
+							control={form.control}
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>URL</FormLabel>
+									<FormControl>
+										<Input {...field} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							name='description'
+							control={form.control}
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>Description</FormLabel>
+									<FormControl>
+										<Textarea {...field} rows={6} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<DialogFooter className='mt-4'>
+							<DialogClose asChild>
+								<Button variant='secondary'>Close</Button>
+							</DialogClose>
+							<Button variant='default' type='submit'>Save</Button>
+						</DialogFooter>
+					</form>
+				</Form>
 			</DialogContent>
 		</Dialog>
 	)
 }
+
+const schema = z.object({
+	title: z.string(),
+	description: z.string(),
+	url: z.string()
+})
